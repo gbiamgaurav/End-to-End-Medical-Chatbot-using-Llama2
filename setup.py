23136676703645eee382196ccc4adb3a9c8c0b3e
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-12-21 18:50:49 +0530

    requirements and setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup 
+
+setup(
+    name = "Medical Chatbot",
+    version = "0.0.0",
+    author = "gbiamgaurav",
+    author_email = "gaurav.bhattacharya10@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+)
+
